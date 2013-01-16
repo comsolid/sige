dc2709a94087d2bcb2ca5dacd1de334b8e050b5b
Átila Camurça <camurca.home@gmail.com>
2013-01-16 18:18:20 +0000

    > permitir <a> com target _blank no resumo do evento.

--- sige-comsolid/library/Sige/Filter/HTMLPurifier.php
+++ sige-comsolid/library/Sige/Filter/HTMLPurifier.php
@@ -12,11 +12,12 @@ class Sige_Filter_HTMLPurifier implements Zend_Filter_Interface {
    protected $purifier;
 
    public function __construct($options = null) {
+      // lista de chaves para $config->set($key, $value)
       HTMLPurifier_Bootstrap::registerAutoload();
       $config = HTMLPurifier_Config::createDefault();
       $config->set('HTML.Strict', true);
       $config->set('Attr.EnableID', true);
-      $config->set('Attr.AllowedFrameTargets', array('_blank' => true));
+      $config->set('HTML.TargetBlank', true);
       $this->purifier = new HTMLPurifier($config);
    }
 
