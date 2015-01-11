944942bfbc42c43fcc88d4fbed694751c4ef0bcc
Átila Camurça <camurca.home@gmail.com>
2015-01-11 16:53:05 -0300

    Tela Adicionar e Editar horários refeita.
    Form movido de Application para Admin. Usando apenas uma view para Adicionar e Editar.

--- application/modules/admin/controllers/HorarioController.php
+++ application/modules/admin/controllers/HorarioController.php
@@ -1,177 +1,143 @@
 <?php
-
 class Admin_HorarioController extends Zend_Controller_Action {
 
-   public function init() {
-      if (!Zend_Auth::getInstance()->hasIdentity()) {
-         $session = new Zend_Session_Namespace();
-         $session->setExpirationSeconds(60 * 60 * 1); // 1 minuto
-         $session->url = $_SERVER['REQUEST_URI'];
-         return $this->_helper->redirector->goToRoute(array(), 'login', true);
-      }
+    public function init() {
+        if (!Zend_Auth::getInstance()->hasIdentity()) {
+            $session = new Zend_Session_Namespace();
+            $session->setExpirationSeconds(60 * 60 * 1); // 1 minuto
+            $session->url = $_SERVER['REQUEST_URI'];
+            return $this->_helper->redirector->goToRoute(array(), 'login', true);
+        }
 
-      $sessao = Zend_Auth::getInstance()->getIdentity();
-      if (!$sessao["administrador"]) {
-         return $this->_helper->redirector->goToRoute(array('controller' => 'participante',
-                     'action' => 'index'), 'default', true);
-      }
-      $this->view->menu = new Application_Form_Menu($this->view, 'admin', true);
-   }
+        $sessao = Zend_Auth::getInstance()->getIdentity();
+        if (!$sessao["administrador"]) {
+            return $this->_helper->redirector->goToRoute(array('controller' => 'participante', 'action' => 'index'), 'default', true);
+        }
 
-   public function criarAction() {
-      $idEvento = $this->_request->getParam('id');
+        $this->_helper->layout->setLayout('twbs3-admin/layout');
+        $this->view->menu = new Sige_Desktop_AdminSidebarLeftMenu($this->view, 'events');
+    }
 
-      $form = new Application_Form_Horarios();
-      $form->cria();
-      $this->view->form = $form;
+    public function criarAction() {
+        $this->view->title = _('New schedule');
+        $this->_helper->viewRenderer->setRender('salvar');
 
-      $model = new Application_Model_EventoRealizacao();
-      $select = "SELECT TO_CHAR(data, 'DD/MM/YYYY') as data, TO_CHAR(hora_inicio, 'HH24:MI') as hora_inicio, TO_CHAR(hora_fim, 'HH24:MI') as hora_fim FROM evento_realizacao WHERE id_evento = ?";
-
-      $this->view->idEvento = $idEvento;
-      $this->view->horariosEventos = $model->getAdapter()->fetchAll($select, $idEvento);
-
-      $data = $this->getRequest()->getPost();
-      if ($this->getRequest()->isPost() && $form->isValid($data)) {
-         $data = $form->getValues();
-         $data['id_evento'] = $idEvento;
-
-         try {
-            $sessao = Zend_Auth::getInstance()->getIdentity();
-            $idEncontro = $sessao ["idEncontro"];
-            $existe = $model->existeHorario(array(
-                $idEncontro,
-                $data['id_sala'],
-                $data['data'],
-                $data['hora_inicio'],
-                $data['hora_fim']
-            ));
-
-            if (! $existe) {
-               $model->insert($data);
-               return $this->_helper->redirector->goToRoute(array(
-                           'module' => 'admin',
-                           'controller' => 'evento',
-                           'action' => 'detalhes',
-                           'id' => $idEvento), 'default', true);
-            } else {
-               $this->_helper->flashMessenger->addMessage(
-                     array('danger' => 'Já existe um evento no mesmo dia, mesma sala e mesmo horário.'));
-            }
-         } catch (Exception $e) {
-            $this->_helper->flashMessenger->addMessage(
-                     array('danger' => 'Ocorreu um erro inesperado.<br/>Detalhes: '
-                         . $e->getMessage()));
-         }
-      }
-   }
-
-   public function editarAction() {
-      $form = new Application_Form_Horarios();
-      $form->cria();
-      $this->view->form = $form;
-
-      $model = new Application_Model_EventoRealizacao();
-      $evento = $this->_request->getParam('evento', 0);
-
-      if ($this->getRequest()->isPost()) {
-         $formData = $this->getRequest()->getPost();
-         if ($form->isValid($formData)) {
-            $id = $this->_request->getParam('id', 0);
+        $idEvento = $this->_request->getParam('id');
+        $form = new Admin_Form_Horarios();
+        //$form->cria();
+        $this->view->form = $form;
+        $model = new Application_Model_EventoRealizacao();
+        $select = "SELECT TO_CHAR(data, 'DD/MM/YYYY') as data,
+            TO_CHAR(hora_inicio, 'HH24:MI') as hora_inicio,
+            TO_CHAR(hora_fim, 'HH24:MI') as hora_fim, descricao
+            FROM evento_realizacao WHERE id_evento = ? ORDER BY data ASC, hora_inicio ASC";
+        $this->view->idEvento = $idEvento;
+        $this->view->horariosEventos = $model->getAdapter()->fetchAll($select, $idEvento);
+        $data = $this->getRequest()->getPost();
+        if ($this->getRequest()->isPost() && $form->isValid($data)) {
             $data = $form->getValues();
-
+            $data['id_evento'] = $idEvento;
             try {
-               $sessao = Zend_Auth::getInstance()->getIdentity();
-               $idEncontro = $sessao ["idEncontro"];
-               $existe = $model->existeHorario(array(
-                  $idEncontro,
-                  $data['id_sala'],
-                  $data['data'],
-                  $data['hora_inicio'],
-                  $data['hora_fim']
-               ));
-
-               // se id for igual ao id existe, pode atualizar, pois se trata do mesmo evento
-               if (! $existe or ($id == $existe)) {
-                  $model->update($data, 'evento = ' . $id);
-                  return $this->_helper->redirector->goToRoute(array(
-                              'module' => 'admin',
-                              'controller' => 'evento',
-                              'action' => 'detalhes',
-                              'id' => $evento), 'default', true);
-               } else {
-                  $this->_helper->flashMessenger->addMessage(
-                     array('danger' => 'Já existe um evento no mesmo dia, mesma sala e mesmo horário.'));
-               }
-            } catch (Exception $e) {
-               $this->_helper->flashMessenger->addMessage(
-                     array('danger' => 'Ocorreu um erro inesperado.<br/>Detalhes: '
-                         . $e->getMessage()));
+                $sessao = Zend_Auth::getInstance()->getIdentity();
+                $idEncontro = $sessao["idEncontro"];
+                $existe = $model->existeHorario(array($idEncontro, $data['id_sala'], $data['data'], $data['hora_inicio'], $data['hora_fim']));
+                if (!$existe) {
+                    $model->insert($data);
+                    $this->_helper->flashMessenger->addMessage(array('success' => 'Horário adicionado com sucesso.'));
+                    return $this->_helper->redirector->goToRoute(array('module' => 'admin', 'controller' => 'evento', 'action' => 'detalhes', 'id' => $idEvento), 'default', true);
+                } else {
+                    $this->_helper->flashMessenger->addMessage(array('danger' => 'Já existe um evento no mesmo dia, mesma sala e mesmo horário.'));
+                }
             }
-         } else {
-            $form->populate($formData);
-         }
-      } else {
-         $id = $this->_getParam('id', 0);
-         if ($id > 0) {
-            $form->populate($model->getAdapter()->fetchRow("SELECT evento, id_evento,
+            catch(Exception $e) {
+                $this->_helper->flashMessenger->addMessage(array('danger' => 'Ocorreu um erro inesperado.<br/>Detalhes: ' . $e->getMessage()));
+            }
+        }
+    }
+
+    public function editarAction() {
+        $this->view->title = _('Edit schedule');
+        $this->_helper->viewRenderer->setRender('salvar');
+
+        $form = new Admin_Form_Horarios();
+        $this->view->form = $form;
+
+        $model = new Application_Model_EventoRealizacao();
+        $evento = $this->_request->getParam('evento', 0);
+
+        if ($this->getRequest()->isPost()) {
+            $formData = $this->getRequest()->getPost();
+            if ($form->isValid($formData)) {
+                $id = $this->_request->getParam('id', 0);
+                $data = $form->getValues();
+                try {
+                    $sessao = Zend_Auth::getInstance()->getIdentity();
+                    $idEncontro = $sessao["idEncontro"];
+                    $existe = $model->existeHorario(array($idEncontro, $data['id_sala'], $data['data'], $data['hora_inicio'], $data['hora_fim']));
+                    // se id for igual ao id existe, pode atualizar, pois se trata do mesmo evento
+                    if (!$existe or ($id == $existe)) {
+                        $model->update($data, 'evento = ' . $id);
+                        $this->_helper->flashMessenger->addMessage(array('success' => 'Horário atualizado com sucesso.'));
+                        return $this->_helper->redirector->goToRoute(array('module' => 'admin', 'controller' => 'evento', 'action' => 'detalhes', 'id' => $evento), 'default', true);
+                    } else {
+                        $this->_helper->flashMessenger->addMessage(array('danger' => 'Já existe um evento no mesmo dia, mesma sala e mesmo horário.'));
+                    }
+                }
+                catch(Exception $e) {
+                    $this->_helper->flashMessenger->addMessage(array('danger' => 'Ocorreu um erro inesperado.<br/>Detalhes: ' . $e->getMessage()));
+                }
+            } else {
+                $form->populate($formData);
+            }
+        } else {
+            $id = $this->_getParam('id', 0);
+            if ($id > 0) {
+                $form->populate($model->getAdapter()->fetchRow("SELECT evento, id_evento,
                   id_sala, TO_CHAR(data, 'DD/MM/YYYY') as data,
                   TO_CHAR(hora_inicio, 'HH24:MI') as hora_inicio,
                   TO_CHAR(hora_fim, 'HH24:MI') as hora_fim, descricao
                FROM evento_realizacao WHERE evento = " . $id));
-         }
-      }
-
-      $select = "SELECT TO_CHAR(data, 'DD/MM/YYYY') as data,
-         TO_CHAR(hora_inicio, 'HH24:MI') as hora_inicio,
-         TO_CHAR(hora_fim, 'HH24:MI') as hora_fim FROM evento_realizacao
-         WHERE id_evento = ?";
-      $this->view->idEvento = $evento;
-      $this->view->horariosEventos = $model->getAdapter()->fetchAll($select, $evento);
-   }
-
-   public function deletarAction() {
-      $model = new Admin_Model_EventoRealizacao();
-      $evento = $this->_request->getParam('evento', 0);
-
-      if ($this->getRequest()->isPost()) {
-         $del = $this->getRequest()->getPost('del');
-         $id = (int) $this->getRequest()->getPost('id');
-         if (!isset($id)) {
-            $this->_helper->flashMessenger->addMessage(
-                    array('danger' => 'Horário não encontrado.'));
-         } else if ($del == "confimar") {
-            try {
-               $where = array(
-                   "evento = ?" => $id
-               );
-               $model->delete($where);
-               $this->_helper->flashMessenger->addMessage(
-                        array('success' => 'Horário deletado com sucesso.'));
-            } catch (Zend_Db_Exception $e) {
-               if ($e->getCode() == 23503){
-                  $this->_helper->flashMessenger->addMessage(
-                          array('danger' => 'Esse hórario não pode ser removido, pois já existem participantes inscritos.'));
-               } else {
-                  $this->_helper->flashMessenger->addMessage(
-                        array('danger' => 'Ocorreu um erro inesperado.<br/>Detalhes: '
-                              . $e->getMessage()));
-               }
             }
-         }
-         return $this->_helper->redirector->goToRoute(array(
-                     'module' => 'admin',
-                     'controller' => 'evento',
-                     'action' => 'detalhes',
-                     'id' => $evento), 'default', true);
-      } else {
-         $id = $this->_getParam('id', 0);
-         try {
-            $this->view->horario = $model->ler($id);
-         } catch (Exception $e) {
-            $this->_helper->flashMessenger->addMessage(
-                    array('danger' => $e->getMessage()));
-         }
-      }
-   }
+        }
+        $select = "SELECT TO_CHAR(data, 'DD/MM/YYYY') as data,
+         TO_CHAR(hora_inicio, 'HH24:MI') as hora_inicio,
+         TO_CHAR(hora_fim, 'HH24:MI') as hora_fim, descricao FROM evento_realizacao
+         WHERE id_evento = ? ORDER BY data ASC, hora_inicio ASC";
+        $this->view->idEvento = $evento;
+        $this->view->horariosEventos = $model->getAdapter()->fetchAll($select, $evento);
+    }
+
+    public function deletarAction() {
+        $model = new Admin_Model_EventoRealizacao();
+        $evento = $this->_request->getParam('evento', 0);
+        if ($this->getRequest()->isPost()) {
+            $del = $this->getRequest()->getPost('del');
+            $id = (int)$this->getRequest()->getPost('id');
+            if (!isset($id)) {
+                $this->_helper->flashMessenger->addMessage(array('danger' => 'Horário não encontrado.'));
+            } else if ($del == "confimar") {
+                try {
+                    $where = array("evento = ?" => $id);
+                    $model->delete($where);
+                    $this->_helper->flashMessenger->addMessage(array('success' => 'Horário deletado com sucesso.'));
+                }
+                catch(Zend_Db_Exception $e) {
+                    if ($e->getCode() == 23503) {
+                        $this->_helper->flashMessenger->addMessage(array('danger' => 'Esse hórario não pode ser removido, pois já existem participantes inscritos.'));
+                    } else {
+                        $this->_helper->flashMessenger->addMessage(array('danger' => 'Ocorreu um erro inesperado.<br/>Detalhes: ' . $e->getMessage()));
+                    }
+                }
+            }
+            return $this->_helper->redirector->goToRoute(array('module' => 'admin', 'controller' => 'evento', 'action' => 'detalhes', 'id' => $evento), 'default', true);
+        } else {
+            $id = $this->_getParam('id', 0);
+            try {
+                $this->view->horario = $model->ler($id);
+            }
+            catch(Exception $e) {
+                $this->_helper->flashMessenger->addMessage(array('danger' => $e->getMessage()));
+            }
+        }
+    }
 }
---
+++ application/modules/admin/forms/Horarios.php
@@ -0,0 +1,146 @@
+<?php
+
+class Admin_Form_Horarios extends Zend_Form {
+
+    private  $descricao;
+    const HORA_INI = 'hora_inicio';
+    const HORA_FIM = 'hora_fim';
+
+
+    /**
+    * @return the $descricao
+    */
+    public function getDescricao() {
+        return $this->descricao;
+    }
+
+    /**
+    * @param field_type $descricao
+    */
+    public function setDescricao($descricao) {
+        $this->descricao = $descricao;
+    }
+
+    public function init(){
+        $this->setMethod("post");
+
+        $this->addElement($this->_descricao())
+        ->addElement($this->_id_sala())
+        ->addElement($this->_data())
+        ->addElement($this->_hora(self::HORA_INI, 'Horário Inicio:'))
+        ->addElement($this->_hora(self::HORA_FIM, 'Horário Término:'));
+
+        $submit = new Zend_Form_Element_Submit('submit');
+        $submit->setLabel(_("Confirm"))
+            ->setAttrib('id', 'submitbutton')
+            ->setAttrib('class', 'btn btn-primary');
+        $submit->setDecorators(array(
+            'ViewHelper',
+            'Description',
+            'Errors',
+        ));
+        $this->addElement($submit);
+    }
+
+    protected function _descricao() {
+        //$descricao = $this->createElement('text', 'descricao', array('label' => 'Descrição: '));
+        //$descricao->setValue($this->getDescricao())
+        //->setAttrib('placeholder', 'Turma 1 ou Parte 1...');
+
+        $e = new Zend_Form_Element_Text('descricao');
+        $e->setLabel(_('Description:'));
+        //$e->setRequired(true);
+        $e->addValidator('StringLength', false, array(1,100));
+        //$e->setAttrib("data-required", "true");
+        $e->setAttrib("data-rangelength", "[1,100]");
+        $e->addFilter('StripTags');
+        $e->addFilter('StringTrim');
+        $e->setAttrib('class', 'form-control');
+        $e->setAttrib('placeholder', _('Class 1 or Part 1...'));
+
+        $e->setDecorators(array(
+            'ViewHelper',
+            'Description',
+            'Errors',
+            array('HtmlTag', ''),
+            array('Label', ''),
+        ));
+        return $e;
+    }
+
+    protected function _id_sala() {
+        /*$salas = new Application_Model_Sala();
+        $salas->fetchAll();
+
+        $salasForm = $this->createElement('select', 'id_sala', array('label'=>'Salas: '));
+
+        foreach ($salas->fetchAll() as $sala){
+            $salasForm->addMultiOptions(array($sala->id_sala => $sala->nome_sala));
+        }*/
+
+        $e = new Zend_Form_Element_Select('id_sala');
+        $e->setRequired(true);
+        $e->setLabel(_('Place:'));
+        $e->setAttrib('class', 'form-control');
+        $model = new Application_Model_Sala();
+        $rs = $model->fetchAll();
+        foreach ($rs as $item) {
+            $e->addMultiOption($item->id_sala, $item->nome_sala);
+        }
+
+        $e->setDecorators(array(
+            'ViewHelper',
+            'Description',
+            'Errors',
+            array('HtmlTag', ''),
+            array('Label', ''),
+        ));
+        return $e;
+    }
+
+    protected function _data() {
+        $model = new Application_Model_Encontro();
+        $sessao = Zend_Auth::getInstance()->getIdentity();
+        $where = $model->getAdapter()->quoteInto('id_encontro = ?', $sessao["idEncontro"]);
+        $row = $model->fetchRow($where);
+        $element = $this->createElement('radio', 'data', array('label' => 'Data: '));
+        $data_ini = new Zend_Date($row->data_inicio);
+        $data_fim = new Zend_Date($row->data_fim);
+        while ($data_ini <= $data_fim) {
+            $element->addMultiOption($data_ini->toString('dd/MM/YYYY'), $data_ini->toString('dd/MM/YYYY'));
+            $data_ini->add(1, Zend_Date::DAY);
+        }
+        $element->setRequired(true)->addErrorMessage("Escolha uma data para realização do evento");
+
+        $element->setDecorators(array(
+            'ViewHelper',
+            'Description',
+            'Errors',
+            array('HtmlTag', ''),
+            array('Label', ''),
+        ));
+
+        return $element;
+    }
+
+    protected function _hora($id, $label) {
+        $e = new Zend_Form_Element_Select($id);
+        $e->setRequired(true);
+        $e->setLabel($label);
+        $e->setAttrib('class', 'form-control');
+        $hora_aux = $hora_min = new Zend_Date('08:00', 'HH:mm');
+        $hora_max = new Zend_Date('17:00', 'HH:mm');
+        while ($hora_aux <= $hora_max) {
+            if (self::HORA_INI == $id and $hora_aux == $hora_max) {
+                $hora_aux->add(1, Zend_Date::HOUR);
+                continue;
+            } else if (self::HORA_FIM == $id and $hora_aux == new Zend_Date('08:00', 'HH:mm')) {
+                $hora_aux->add(1, Zend_Date::HOUR);
+                continue;
+            }
+            $e->addMultiOption($hora_aux->toString('HH:mm'), $hora_aux->toString('HH:mm'));
+            $hora_aux->add(1, Zend_Date::HOUR);
+        }
+        return $e;
+    }
+}
