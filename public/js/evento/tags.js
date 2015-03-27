17fff896630e5886b48f8cbc8420569c67f05ff0
Átila Camurça <camurca.home@gmail.com>
2015-03-27 19:17:44 -0300

    Tela Criar/Editar tags do evento atualizada, mostrando de forma correta quando não há tags, mesmo depois de mudar a tela via ajax.

--- public/js/evento/tags.js
+++ public/js/evento/tags.js
@@ -1,46 +1,144 @@
 
 $(function() {
     $('a.close[data-toggle="tooltip"]').tooltip();
-    $("#loading").hide();
-    $("#termo").select();
-   
-    $("#termo").keyup(function(event) {
+
+    var termo = $('#termo')
+        , loading = $('#loading');
+
+    termo.select();
+    loading.hide();
+
+    function appendToUl(id, descricao) {
+        if ($('ul.list-group li').length === 0) {
+            $('ul.list-group').html('');
+            $('div.panel-body').css('display', 'none');
+        }
+
+        $('<li></li>', {
+            html: descricao +
+                  '<a href="#" onclick="return false;" ' +
+                  'class="close remover pull-right" ' +
+                  sprintf('data-id="%d" data-toggle="tooltip" data-placement="top" ', id) +
+                  sprintf('title="%s">', _('Remove')) +
+                  '<span aria-hidden="true">&times;</span><span class="sr-only">Close</span></a>',
+            class: 'list-group-item',
+            id: 'tag_' + id
+        }).appendTo($('ul.list-group'));
+    }
+
+    /**
+     * Salva uma tag existente referenciando o evento.
+     */
+    function salvar(id, descricao) {
+        if (id > 0) {
+            loading.show();
+            var params = {
+                id_evento: $('#id_evento').val(),
+                id: id
+            };
+            var url = '/evento/ajax-salvar-tag';
+            $.getJSON(url, params, function(json) {
+                if (json.ok) {
+                    alertify.success(json.msg);
+                    appendToUl(id, descricao);
+                    termo.val('').focus();
+                } else if (json.error) {
+                    alertify.error(json.error);
+                    termo.select();
+                }
+            }).complete(function() {
+                loading.hide();
+            });
+        }
+    }
+
+    /**
+     * Cria uma nova tag inserindo na tabela tags.
+     */
+    function criar(descricao) {
+        if (descricao !== '') {
+            loading.show();
+            var params = {
+                descricao:  descricao
+            };
+            var url = '/evento/ajax-criar-tag';
+            $.getJSON(url, params, function(json) {
+                if (json.ok) {
+                    salvar(json.id, descricao);
+                } else if (json.error) {
+                    alertify.error(json.error);
+                    termo.select();
+                }
+            }).complete(function() {
+                loading.hide();
+            });
+        }
+    }
+
+    function deletar(id) {
+        if (id > 0) {
+            loading.show();
+
+            var url = '/evento/ajax-deletar-tag';
+            var params = {
+                id: id,
+                id_evento: $('#id_evento').val()
+            };
+            $.getJSON(url, params, function(json) {
+                if (json.ok) {
+                    alertify.success(json.msg);
+                    $('#tag_' + id).remove();
+                    termo.select();
+                } else if (json.error) {
+                    alertify.error(json.error);
+                    termo.select();
+                }
+            }).complete(function() {
+                loading.hide();
+                if ($('ul.list-group li').length === 0) {
+                    $('div.panel-body').css('display', 'block');
+                }
+            });
+        }
+    }
+
+    $('#termo').keyup(function(event) {
         if ($(this).val().length === 0) {
-            $("#criar-tag").hide();
+            $('#criar-tag').hide();
             return false;
         }
-      
+
         switch (event.keyCode) {
             case 27: // ESC
                 if ($(this).val().length !== 0) {
-                    $("#criar-tag").show();
+                    $('#criar-tag').show();
                 }
                 return false;
             default:
                 return true;
         }
     });
-   
+
     $(document).delegate('#criar-tag', 'click', function() {
-        criar($("#termo").val());
-        $("#criar-tag").hide();
+        criar(termo.val());
+        $('#criar-tag').hide();
     });
-   
+
     $(document).delegate('a.remover', 'click', function() {
-        deletar($(this).attr("data-id"));
+        deletar($(this).attr('data-id'));
     });
-   
-    $("#termo").autocomplete({
-        search: function( event, ui ) {
-            $("#loading").show();
+
+    $('#termo').autocomplete({
+        search: function( /*event, ui*/ ) {
+            loading.show();
         },
         source: function( request, response ) {
-         
+
             $.ajax({
-                url: "/evento/ajax-buscar-tags/termo/" + request.term,
-                contentType: "application/x-www-form-urlencoded; charset=utf-8;",
+                url: '/evento/ajax-buscar-tags/termo/' + request.term,
+                contentType: 'application/x-www-form-urlencoded; charset=utf-8;',
                 success: function( result ) {
-                    $("#criar-tag").hide();
+                    $('#criar-tag').hide();
                     if (result.itens.length > 0) {
                         response($.map(result.itens, function(item) {
                             return {
@@ -50,12 +148,12 @@ $(function() {
                             };
                         })); // end of response
                     } else {
-                        $("#criar-tag").show();
-                        $("#termo").autocomplete('close');
+                        $('#criar-tag').show();
+                        termo.autocomplete('close');
                     }
                 } // end of success
             }).complete(function () {
-                $("#loading").hide();
+                loading.hide();
             }); // end of ajax
         }, // end of source
         minLength: 1,
@@ -63,102 +161,5 @@ $(function() {
             salvar(ui.item.id, ui.item.value);
         }
     });
-   
+
 });
-
-function appendToUl(id, descricao) {
-    if ($("ul.list-group li").length === 0) {
-        $("ul.list-group").html("");
-    }
-    
-    /**
-     * <li class="list-group-item" id="tag_<?= $tag['id'] ?>">
-            <?= $tag['descricao'] ?>
-            <a href="#" onclick="return false;"
-                class="close remover pull-right"
-                data-id="<?= $tag['id'] ?>"
-                data-toggle="tooltip" data-placement="top"
-                title="<?php echo $this->translate("Remove"); ?>">
-               <span aria-hidden="true">&times;</span>
-               <span class="sr-only">Close</span>
-            </a>
-        </li>
-     */
-   
-    $("<li>", {
-        html: descricao +
-              '<a href="#" onclick="return false;" ' +
-              'class="close remover pull-right" ' +
-              sprintf('data-id="%d" data-toggle="tooltip" data-placement="top" ', id) +
-              sprintf('title="%s">', _('Remove')) +
-              '<span aria-hidden="true">&times;</span><span class="sr-only">Close</span></a>',
-        class: "list-group-item",
-        id: "tag_" + id
-    }).appendTo($("ul.list-group"));
-}
-
-/**
- * Salva uma tag existente referenciando o evento.
- */
-function salvar(id, descricao) {
-    if (id > 0) {
-        $("#loading").show();
-        var id_evento = $("#id_evento").val();
-        var url = "/evento/ajax-salvar-tag/id/" + id + "/id_evento/" + id_evento;
-        $.getJSON(url, function(json) {
-            if (json.ok) {
-                //mostrarMensagem("div.success", json.msg);
-                alertify.success(json.msg);
-                appendToUl(id, descricao);
-                $("#termo").val("");
-                $("#termo").focus();
-            } else if (json.error) {
-                //mostrarMensagem("div.error", json.erro);
-                alertify.error(json.error);
-                $("#termo").select();
-            }
-        }).complete(function() {
-            $("#loading").hide();
-        });
-    }
-}
-
-/**
- * Cria uma nova tag inserindo na tabela tags.
- */
-function criar(descricao) {
-    if (descricao !== "") {
-        $("#loading").show();
-        var url = "/evento/ajax-criar-tag/descricao/" + descricao;
-        $.getJSON(url, function(json) {
-            if (json.ok) {
-                salvar(json.id, descricao);
-            } else if (json.error) {
-                alertify.error(json.error);
-                $("#termo").select();
-            }
-        }).complete(function() {
-            $("#loading").hide();
-        });
-    }
-}
-
-function deletar(id) {
-    if (id > 0) {
-        $("#loading").show();
-        var id_evento = $("#id_evento").val();
-        var url = "/evento/ajax-deletar-tag/id/" + id + "/id_evento/" + id_evento;
-        $.getJSON(url, function(json) {
-            if (json.ok) {
-                alertify.success(json.msg);
-                $("#tag_" + id).remove();
-                $("#termo").select();
-            } else if (json.error) {
-                alertify.error(json.error);
-                $("#termo").select();
-            }
-        }).complete(function() {
-            $("#loading").hide();
-        });
-    }
-}
